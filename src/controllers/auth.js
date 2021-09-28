feaf5011ccda94ecb9283c26a92192ee65791840
Paulo Cesar <paulocitron99@gmail.com>
2021-09-28 00:06:19 -0300

    fix: remove double handle headers

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -13,26 +13,27 @@ exports.login = (req, res) => {
       if (error || results.rowCount == 0) {
         return res.status(401).json({ status: 'error', 
         message: 'Email not found!' })
+      } else {
+        user = results.rows[0]
+
+        if (user.password !== password) {
+          res.status(400).json({
+            status: 'error',
+            message: 'Invalid password!'
+          })
+        } else {
+          const token = jwt.sign(
+            user,
+            process.env.JWT_SECRET,
+            {
+              expiresIn: '1h'
+            })
+            return res.status(200).json({
+              message: 'Auth successful!',
+              token: token
+            })
+        }
       }
-      user = results.rows[0]
     }
   )
-
-  if (user.password !== password) {
-    res.status(400).json({
-      status: 'error',
-      message: 'Invalid password!'
-    })
-  } else {
-    const token = jwt.sign(
-      user,
-      process.env.JWT_SECRET,
-      {
-        expiresIn: '1h'
-      })
-      return res.status(200).json({
-        message: 'Auth successful!',
-        token: token
-      })
-  }
 }
